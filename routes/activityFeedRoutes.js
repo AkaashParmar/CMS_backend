f4b0e00a4edbc46491ab97f4218381c46dc256d0
mohitkumar0831 <mohitkumar@netfotech.in>
2025-09-10 22:42:57 +0530

    added count api, companyAdmin

--- routes/activityFeedRoutes.js
+++ routes/activityFeedRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getActivityFeed, getMonthlyBillingTrend } from "../controllers/activityFeedController.js";
+import { getActivityFeed, getMonthlyBillingTrend, getSummaryStats } from "../controllers/activityFeedController.js";
 
 const router = express.Router();
 
 router.get("/", getActivityFeed);
 router.get("/monthly-billing", getMonthlyBillingTrend);
+router.get("/summary-stats", getSummaryStats);
 
 export default router;
