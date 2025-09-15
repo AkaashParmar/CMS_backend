f70d553c09ec345c916209c88d43b02ab650e07b
mohitkumar0831 <mohitkumar@netfotech.in>
2025-09-15 10:17:38 +0530

    updated patientreport and prescription

--- models/Report.js
+++ models/Report.js
@@ -5,18 +5,21 @@ const patientReportSchema = new mongoose.Schema(
     name: { type: String, required: true },
     fileUrl: { type: String },
     date: { type: String, required: true },
-    // doctor: {
-    //   type: mongoose.Schema.Types.ObjectId,
-    //   ref: "User", // Doctor's User model
-    //   required: true,
-    // },
-    doctorName: { type: String, required: true },
+
+    // Reference to the Doctor selected from dropdown
+    doctor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // Make sure the User model has role 'doctor'
+      required: true,
+    },
+
     comments: { type: String, default: "" },
     status: {
       type: String,
       enum: ["Pending Review", "Reviewed"],
       default: "Pending Review",
     },
+
     uploadedBy: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
--- routes/prescriptionRoutes.js
+++ routes/prescriptionRoutes.js
@@ -3,6 +3,7 @@ import {
   createPrescription,
   getPrescriptions,
   getPrescriptionById,
+  getRecentPrescriptions,
 } from "../controllers/prescriptionController.js";
 import authenticate from "../middleware/authmiddleware.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/create", authenticate, createPrescription);
 router.get("/get", authenticate, getPrescriptions);
 router.get("/get/:id", authenticate, getPrescriptionById);
+router.get("/recent/:patientId", authenticate, getRecentPrescriptions);
 
 export default router;
